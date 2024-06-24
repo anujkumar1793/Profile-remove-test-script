e5323df751af95916ed1a854786ea42c7eb12ddc
Anuj Kumar <anujkumar4200000@gmail.com>
2024-06-24 08:37:44 +0530

    Add files via upload

---
+++ User profile deleting test script.ps1
@@ -0,0 +1,17 @@
+﻿$computerName = "DINM5CD32862R9"
+$profilePath = "C:\Users"
+
+$profilesToExclude = @("defaultuser0","Public","z004zbam","administrator","admin")
+
+$allprofiles = Get-WmiObject Win32_UserProfile -ComputerName $computerName | Where-Object { $_.special -eq $false }
+
+foreach ($profile in $allprofiles){
+    $UserName = $profile.localPath.Split('\')[-1]
+
+    if($profilesToExclude -notcontains $UserName){
+    Write-Host "Deleting profile for$UserName"
+    Remove-Item -Path "$profilePath\UserName" -Force -Recurse
+    }else {
+    Write-Host -BackgroundColor Green "Excluding profile for $UserName from delection"
+    }
+}
---
+++ User profile deleting test script2.ps1
@@ -0,0 +1,17 @@
+﻿$computerName = "DINM5CD32862R9"
+$profilePath = "C:\Users"
+
+$profilesToExclude = @("defaultuser0", "Public", "z004zbam", "administrator","admin")
+
+$allProfiles = Get-WmiObject Win32_UserProfile -ComputerName $computerName | Where-Object { $_.Special -eq $false }
+
+foreach ($profile in $allProfiles) {
+    $userName = $profile.LocalPath.Split('\')[-1]
+
+        if ($profilesToExclude -notcontains $userName) { 
+        Write-Host "Deleting profile for $userName"
+        Remove-Item -Path "$profilePath\$userName" -Force -Recurse
+    } else {
+        Write-Host -BackgroundColor Green "Excluding profile for $userName from deletion"
+    }
+}
